92cddb6727c3881538c8e1ffaa218401247badff
Muhammad Ahmad <ranaahmad131@gmail.com>
2025-02-05 19:59:03 +0500

    Workjing on protfolio

---
+++ src/App.js
@@ -0,0 +1,123 @@
+// import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+import "tailwindcss";
+
+
+const pizzaData = [
+  {
+    name: "Behari Kebab",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Chicken Clunker",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Crown Crust",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pepporoni",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Ranch",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Peri Peri",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "prosciutto.jpg",
+    soldOut: false,
+  },
+];
+
+
+
+function App() {
+  
+  const data = pizzaData;
+  
+  return (
+    <div className="container">
+      <Header/>
+      <MenuText/>
+      <Menu data={data}/>
+      <Stats/>
+    </div>
+  );
+}
+
+
+
+
+function Header(){
+  return <div className="header">
+    <h1>Pizza Shop</h1>
+  </div>
+}
+
+function MenuText() {
+  return <div className="menu">
+    <h2>Our Menu</h2>
+    <p>Authentic Pizzas baked professionally</p>
+  </div>
+}
+
+function Menu({data}) {
+  console.log(data)
+  return <div className="pizzas">
+    {data.map(pizza => 
+      <Pizza name={pizza.name} ing={pizza.ingredients} pic={pizza.photoName} price={pizza.price} avalibility={pizza.soldOut} />
+    )}
+  </div>
+}
+
+
+function Pizza({name, pic, ing, price }){
+  return <div className='pizza'>
+    <img src={`/${pic}`} alt={name} />
+
+    
+   <div>
+      <h3>{name}</h3>
+      <p>{ing}</p>
+      <h3>{price}</h3>
+   </div>
+  </div>
+}
+
+
+
+function Stats(){
+  const date = new Date();
+  const h = date.getHours()
+  const open = h >= 12 && h <= 22;
+  console.log(open)
+ return <div className='footer'>
+   <p>
+     {`We are ${open ? "opened, come visit us or order online" : "closed"} `}
+  
+   </p>
+   <button className='btn'>Order Now</button>
+ </div>
+}
+
+export default App;
